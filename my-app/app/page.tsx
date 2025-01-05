100e98f0ae21f37ab0b9cea32bee3a28653b7a0c
joephy527 <joephy527@gmail.com>
2025-01-05 18:37:41 +0300

    first change

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -94,7 +94,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Change made to app
         </a>
       </footer>
     </div>
