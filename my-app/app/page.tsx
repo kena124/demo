6fb1ded44edde0b7732391b45d13761ff482b175
kenabirtu19@gmail.com <kena124>
2025-01-05 16:59:12 +0300

    2nd comit

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
+          <h1>hello</h1>
           <li>Save and see your changes instantly.</li>
         </ol>
 
